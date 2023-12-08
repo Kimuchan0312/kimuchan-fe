39ee28ef45e8b8cedddb29e6819b434c174d5d9a
Kimuchan0312 <75709261+Kimuchan0312@users.noreply.github.com>
2023-12-08 14:19:19 +0700

    update v0.08

--- src/pages/MyProfilePage.js
+++ src/pages/MyProfilePage.js
@@ -1,10 +1,30 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Typography, LinearProgress} from '@mui/material';
-
+import { Card, Typography, LinearProgress } from '@mui/material';
 import axios from 'axios';
 
+const JlptTestResults = ({ lesson }) => {
+  // Assuming lesson.score and lesson.time are properties of the lesson object
+  return (
+    <Card variant="outlined" sx={{ mb: 2, p: 2 }}>
+      <Typography variant="h6">Average Score: </Typography>
+      <LinearProgress variant="determinate" value={lesson.score} />
+      <Typography variant="body2">Average Time: {lesson.time}%</Typography>
+      {lesson.testResults.map((result, index) => (
+        <div key={index}>
+          <p>Test Date: {result.date}</p>
+          <p>Score: {result.score}</p>
+          <p>Time Taken: {result.time}</p>
+          <button onClick={() => {console.log('View answer sheet')}}>
+            View Answer Sheet
+          </button>
+        </div>
+      ))}
+    </Card>
+  );
+};
+
 const MyProfilePage = () => {
-  const [testResults, setTestResults] = useState([]);
+  const [setTestResults] = useState([]);
 
   useEffect(() => {
     // Replace with your API endpoint
@@ -15,27 +35,23 @@ const MyProfilePage = () => {
       .catch(error => {
         console.error('There was an error fetching the test results', error);
       });
-  }, []);
+  }, [setTestResults]);
 
-const JlptTestResults = ({ lesson }) => {
+  const lesson = {
+    score: 75,
+    time: 50,
+    // Assuming testResults is an array of test results
+    testResults: [
+      { date: '2023-01-01', score: 80, time: 45 },
+      { date: '2023-01-05', score: 70, time: 55 },
+    ],
+  };
+
   return (
-    <Card variant="outlined" sx={{ mb: 2, p: 2 }}>
-      <Typography variant="h6">Average Score: </Typography>
-      <LinearProgress variant="determinate" value={lesson.score} />
-      <Typography variant="body2">Average Time: {lesson.time}%</Typography>
-      {testResults.map((result, index) => (
-        <div key={index}>
-          <p>Test Date: {result.date}</p>
-          <p>Score: {result.score}</p>
-          <p>Time Taken: {result.time}</p>
-          <button onClick={() => {/* Function to view answer sheet */}}>
-            View Answer Sheet
-          </button>
-        </div>
-      ))}
-    </Card>
+    <div>
+      <JlptTestResults lesson={lesson} />
+    </div>
   );
 };
-}
 
 export default MyProfilePage;
